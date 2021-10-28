6a56fb886b17b4a420e289376a6f94f93ab419b6
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2021-10-28 20:31:25 -0300

    config renderização estilos SSR

---
+++ front-end/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material'
+import { theme } from '../theme'
+import Head  from 'next/head'
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Quick Wash</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+export default MyApp
